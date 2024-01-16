cbf4e62f3e6fbff9fa2c024197767a237fcc1365
Simon Nikolaidis <62617399+Si-Ni@users.noreply.github.com>
2024-01-16 16:01:00 +0100

    fixed formatting

--- Scripts/CheckIfURLsOnWebsiteAreValid.sh
+++ Scripts/CheckIfURLsOnWebsiteAreValid.sh
@@ -6,7 +6,7 @@ NC='\033[0m'
 # Check if the number of command line parameters is not equal to 1
 if [ $# -ne 1 ]; then
 	echo -e "${RED}Error: Exactly one parameter is required (URL)"
-    exit 1
+    	exit 1
 fi
 
 URL=$1
@@ -17,8 +17,8 @@ curl -o /tmp/curledHTMLPage.html $1 >> /dev/null 2>&1
 html_file="/tmp/curledHTMLPage.html"
 
 if [ ! -f "$html_file" ]; then
-  echo -e "${RED}Error: HTML file not found.${NC}"
-  exit 1
+	echo -e "${RED}Error: HTML file not found.${NC}"
+	exit 1
 fi
 
 links=$(grep -o 'href="h[^"]*"' "$html_file" | sed 's/href="//;s/"//')
@@ -26,12 +26,12 @@ links=$(grep -o 'href="h[^"]*"' "$html_file" | sed 's/href="//;s/"//')
 echo -e "${NC}Checking links...\n"
 
 for link in $links; do
-  wget --spider -q "$link"
-  status=$?
+	wget --spider -q "$link"
+	status=$?
 
-  echo -e "${NC}Checking link: $link"
+	echo -e "${NC}Checking link: $link"
 
-  if [ $status -ne 0 ]; then
-    echo -e "${RED}Invalid link: $link${NC}"
-  fi
+  	if [ $status -ne 0 ]; then
+    		echo -e "${RED}Invalid link: $link${NC}"
+  	fi
 done
